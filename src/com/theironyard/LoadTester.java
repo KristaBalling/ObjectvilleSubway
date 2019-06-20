7606a1ae38c9e8065b021b1d7d95491f8d99c5c4
Krista Blachian <krista.blachian@scifusion360.com>
2019-06-20 09:00:53 -0400

    TestLoader class complete

--- src/com/theironyard/LoadTester.java
+++ src/com/theironyard/LoadTester.java
@@ -1,5 +1,36 @@
 package com.theironyard;
 
+import java.io.File;
+
 public class LoadTester {
-    
+    public static void main(String[] args) {
+        try {
+            SubwayLoader loader = new SubwayLoader();
+            Subway objectville =
+                    loader.loadFromFile(new File("ObjectvilleSubway.txt"));
+            System.out.println("Testing stations...");
+            if(objectville.hasStation("DRY Drive") &&
+            objectville.hasStation("Weather-O-Rama, Inc.") &&
+            objectville.hasStation("Boards 'R' Us")) {
+                System.out.println("...station test passed successfully.");
+            }else{
+                System.out.println("...station test FAILED.");
+                System.exit(-1);
+            }
+
+            System.out.println("\nTesting connections...");
+            if(objectville.hasConnection("Weather-O-Rama, Inc.",
+                    "XHTML Expressway", "Wirfs-Brock Line") &&
+            objectville.hasConnection("head First Theater",
+                    "Infinite Circle", "Rumbaugh Line")) {
+                System.out.println("...connections test passed successfully.");
+            } else {
+                System.out.println("...connections test FAILED.");
+                System.exit(-1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
 }
